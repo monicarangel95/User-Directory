61887958beda41d7512b1952d0b189220ffdca3d
Monica Rangel <monicarangel95@github.com>
2020-10-12 22:49:45 -0700

    updating index app files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Search from "./pages/Search";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Wrapper from './components/Wrapper';
+import "./App.css";
 
 function App() {
+  document.title = "Employee Directory";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Wrapper>
+        <Search />
+      </Wrapper>
+      <Footer />
     </div>
   );
 }
-
 export default App;
