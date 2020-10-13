1e412c93f7afd32627d5cbfc72c4ef945a0e716d
Monica Rangel <monicarangel95@github.com>
2020-10-12 22:50:59 -0700

    Create index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer navbar-dark bg-dark">
+            <span>Copyright © 2020</span>
+        </footer>
+    );
+}
+
+export default Footer;
