ea875678b92395486dc41a5495532619cd785f55
Monica Rangel <monicarangel95@github.com>
2020-10-12 22:50:23 -0700

    Create index.js

---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Navbar() {
+    return (
+        <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        </div>
+    );
+}
+
+export default Navbar;
